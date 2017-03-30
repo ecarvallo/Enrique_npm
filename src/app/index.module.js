258010a84a416236de660ffda1df5e00d0056f8e
ecarvallo <enriqueimmontemar@gmail.com>
2017-03-30 16:43:13 -0300

    Inicial

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('enriqueNpm', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ui.bootstrap', 'toastr']);
+
+})();
